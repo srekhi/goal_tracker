f1d85b6ccc2d4d81280a579aad6ac73379efc322
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-07-14 22:58:35 -0500

    Set up auto-generation of test files + Guardfile

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module GoalTracker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
-      g.test_framework :rspec, # use 
-        :fixtures => false,
+      g.test_framework :rspec, # use rspec for testing framework
+        :fixtures => false, # 
         :view_specs => false,
         :helper_specs => false,
         :routing_specs => false,
